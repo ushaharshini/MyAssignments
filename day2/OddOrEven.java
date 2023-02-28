29f8dad4b6560a3064fb47da1c2d99611b7b8f70
ushaharshini <125808351+ushaharshini@users.noreply.github.com>
2023-02-28 16:30:57 +0400

    Add files via upload

---
+++ day2/OddOrEven.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+
+public class OddOrEven {
+public static void main(String[] args) {
+
+	
+for (int i=0;i<=20;i++)
+{
+	
+	if(i%2!=0) {
+		System.out.println(i);
+	}
+	}
+	
+}
+}
